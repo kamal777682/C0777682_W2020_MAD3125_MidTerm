cf03493a3f1ba23391354ea57ebb6a134ad269e2
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-08 15:24:34 -0400

    [kamal] title set

--- app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/PersonInfoActivity.java
+++ app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/PersonInfoActivity.java
@@ -1,12 +1,17 @@
 package com.kamal.c0777682_w2020_mad3125_midterm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class PersonInfoActivity extends AppCompatActivity {
 
+    private static final String TAG = PersonInfoActivity.class.getSimpleName();
+    int x = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,4 +19,19 @@ public class PersonInfoActivity extends AppCompatActivity {
         this.setTitle("Person Information Form");
 
     }
-}
+
+        @Override
+        public void onConfigurationChanged(@NonNull Configuration newConfig)
+        {
+            super.onConfigurationChanged(newConfig);
+
+            if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+            {
+                Log.d(TAG, "ORIENTATION_LANDSCAPE " + x );
+            }else
+            {
+                Log.d(TAG, "ORIENTATION_PORTRAIT " + x);
+            }
+        }
+    }
+

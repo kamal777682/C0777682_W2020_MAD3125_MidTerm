536382da99f0670ef523a58cd34fd2f81c8c7611
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-08 11:21:54 -0400

    [kamal] title set

--- app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/PersonInfoActivity.java
+++ app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/PersonInfoActivity.java
@@ -3,6 +3,7 @@ package com.kamal.c0777682_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PersonInfoActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class PersonInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Person Information Form");
+
     }
 }

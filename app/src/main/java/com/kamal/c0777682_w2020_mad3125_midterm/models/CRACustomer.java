4caeba6a0e035c927498a11f8b8b485e73fbcec9
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-09 13:27:45 -0400

    Passing Data

--- app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/models/CRACustomer.java
+++ app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/models/CRACustomer.java
@@ -8,17 +8,10 @@ public class CRACustomer implements Serializable {
     private String lname;
     private String dob;
     private String gender;
-    private String age;
     private String txDate;
     private String grossInc;
-    private String fedTax;
-    private String provTax;
-    private String cpp;
-    private String empIns;
     private String rrsp;
-    private String fwdRRSP;
-    private String totalTaxableInc;
-    private String taxpayed;
+
 
     public CRACustomer(String sinNo, String fname, String lname, String dob, String gender, String txDate,
                        String grossInc, String rrsp) {
@@ -27,17 +20,9 @@ public class CRACustomer implements Serializable {
         this.lname = lname;
         this.dob = dob;
         this.gender = gender;
-        this.age = age;
         this.txDate = txDate;
         this.grossInc = grossInc;
-        this.fedTax = fedTax;
-        this.provTax = provTax;
-        this.cpp = cpp;
-        this.empIns = empIns;
         this.rrsp = rrsp;
-        this.fwdRRSP = fwdRRSP;
-        this.totalTaxableInc = totalTaxableInc;
-        this.taxpayed = taxpayed;
     }
 
     public String getSinNo() {
@@ -49,7 +34,7 @@ public class CRACustomer implements Serializable {
     }
 
     public String getFname() {
-        return fname ;
+        return fname;
     }
 
     public void setFname(String fname) {
@@ -80,14 +65,6 @@ public class CRACustomer implements Serializable {
         this.gender = gender;
     }
 
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
     public String getTxDate() {
         return txDate;
     }
@@ -104,38 +81,6 @@ public class CRACustomer implements Serializable {
         this.grossInc = grossInc;
     }
 
-    public String getFedTax() {
-        return fedTax;
-    }
-
-    public void setFedTax(String fedTax) {
-        this.fedTax = fedTax;
-    }
-
-    public String getProvTax() {
-        return provTax;
-    }
-
-    public void setProvTax(String provTax) {
-        this.provTax = provTax;
-    }
-
-    public String getCpp() {
-        return cpp;
-    }
-
-    public void setCpp(String cpp) {
-        this.cpp = cpp;
-    }
-
-    public String getEmpIns() {
-        return empIns;
-    }
-
-    public void setEmpIns(String empIns) {
-        this.empIns = empIns;
-    }
-
     public String getRrsp() {
         return rrsp;
     }
@@ -143,28 +88,5 @@ public class CRACustomer implements Serializable {
     public void setRrsp(String rrsp) {
         this.rrsp = rrsp;
     }
-
-    public String getFwdRRSP() {
-        return fwdRRSP;
-    }
-
-    public void setFwdRRSP(String fwdRRSP) {
-        this.fwdRRSP = fwdRRSP;
-    }
-
-    public String getTotalTaxableInc() {
-        return totalTaxableInc;
-    }
-
-    public void setTotalTaxableInc(String totalTaxableInc) {
-        this.totalTaxableInc = totalTaxableInc;
-    }
-
-    public String getTaxpayed() {
-        return taxpayed;
-    }
-
-    public void setTaxpayed(String taxpayed) {
-        this.taxpayed = taxpayed;
-    }
 }
+
--- app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/ui/DataDisplayActivity.java
+++ app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/ui/DataDisplayActivity.java
@@ -9,55 +9,55 @@ import com.kamal.c0777682_w2020_mad3125_midterm.R;
 import com.kamal.c0777682_w2020_mad3125_midterm.models.CRACustomer;
 
 import java.io.Serializable;
-
-public class DataDisplayActivity extends AppCompatActivity implements Serializable {
+import com.kamal.c0777682_w2020_mad3125_midterm.models.TaxCalculator;
+public class DataDisplayActivity extends AppCompatActivity  {
     private TextView sinNo;
     private TextView name;
     private TextView dob;
     private TextView gender;
-   // private TextView age;
+    private TextView age;
     private TextView txDate;
     private TextView grossInc;
-   // private TextView fedTax;
-   // private TextView provTax;
-    //private TextView cpp;
-    //private TextView empIns;
+    private TextView fedTax;
+    private TextView provTax;
+    private TextView cpp;
+    private TextView empIns;
     private TextView rrsp;
-    //private TextView fwdRRSP;
-    //private TextView totalTaxableInc;
-    //private TextView taxpayed;
+    private TextView fwdRRSP;
+    private TextView totalTaxableInc;
+    private TextView taxpayed;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_display);
         sinNo = findViewById(R.id.textView);
         name = findViewById(R.id.textView4);
         dob = findViewById(R.id.textView5);
         gender = findViewById(R.id.textView6);
-       // age = findViewById(R.id.textView7);
+        age = findViewById(R.id.textView7);
         txDate = findViewById(R.id.textView8);
         grossInc = findViewById(R.id.textView9);
-        //fedTax = findViewById(R.id.textView10);
-        //provTax = findViewById(R.id.textView11);
-        //cpp= findViewById(R.id.textView12);
-        //empIns = findViewById(R.id.textView13);
+        fedTax = findViewById(R.id.textView10);
+        provTax = findViewById(R.id.textView11);
+        cpp= findViewById(R.id.textView12);
+        empIns = findViewById(R.id.textView13);
         rrsp  = findViewById(R.id.textView14);
-        //fwdRRSP = findViewById(R.id.textView15);
-        //totalTaxableInc = findViewById(R.id.textView16);
-        //taxpayed = findViewById(R.id.textView17);
+        fwdRRSP = findViewById(R.id.textView15);
+        totalTaxableInc = findViewById(R.id.textView16);
+        taxpayed = findViewById(R.id.textView17);
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_display);
+
         CRACustomer tempObj = (CRACustomer) getIntent().getSerializableExtra("CRACustomerObj");
+        name.setText(tempObj.getLname().toUpperCase()+" "+tempObj.getFname());
         sinNo.setText(tempObj.getSinNo());
-        name.setText(tempObj.getLname().toUpperCase() );
-                //+ " "+tempObj.getFname());
         dob.setText(tempObj.getDob());
         gender.setText(tempObj.getGender());
         txDate.setText(tempObj.getTxDate());
         grossInc.setText(tempObj.getGrossInc());
         rrsp.setText(tempObj.getRrsp());
-        //name.setText(tempObj.);
+
 
 
     }

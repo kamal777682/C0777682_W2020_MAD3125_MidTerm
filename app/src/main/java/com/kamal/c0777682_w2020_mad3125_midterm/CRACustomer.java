b03777e35af64d9a8a3458d5cca93c4410723bed
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-04-08 20:02:02 -0400

    Date pickerr adding

--- app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/CRACustomer.java
@@ -1,4 +1,170 @@
 package com.kamal.c0777682_w2020_mad3125_midterm;
 
-public class CRACustomer {
+import java.io.Serializable;
+
+public class CRACustomer implements Serializable {
+    private String sinNo;
+    private String fname;
+    private String lname;
+    private String dob;
+    private String gender;
+    private String age;
+    private String txDate;
+    private String grossInc;
+    private String fedTax;
+    private String provTax;
+    private String cpp;
+    private String empIns;
+    private String rrsp;
+    private String fwdRRSP;
+    private String totalTaxableInc;
+    private String taxpayed;
+
+    public CRACustomer(String sinNo, String fname, String lname, String dob, String gender, String age, String txDate, String grossInc,
+                       String fedTax, String provTax, String cpp, String empIns, String rrsp, String fwdRRSP, String totalTaxableInc, String taxpayed) {
+        this.sinNo = sinNo;
+        this.fname = fname;
+        this.lname = lname;
+        this.dob = dob;
+        this.gender = gender;
+        this.age = age;
+        this.txDate = txDate;
+        this.grossInc = grossInc;
+        this.fedTax = fedTax;
+        this.provTax = provTax;
+        this.cpp = cpp;
+        this.empIns = empIns;
+        this.rrsp = rrsp;
+        this.fwdRRSP = fwdRRSP;
+        this.totalTaxableInc = totalTaxableInc;
+        this.taxpayed = taxpayed;
+    }
+
+    public String getSinNo() {
+        return sinNo;
+    }
+
+    public void setSinNo(String sinNo) {
+        this.sinNo = sinNo;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTxDate() {
+        return txDate;
+    }
+
+    public void setTxDate(String txDate) {
+        this.txDate = txDate;
+    }
+
+    public String getGrossInc() {
+        return grossInc;
+    }
+
+    public void setGrossInc(String grossInc) {
+        this.grossInc = grossInc;
+    }
+
+    public String getFedTax() {
+        return fedTax;
+    }
+
+    public void setFedTax(String fedTax) {
+        this.fedTax = fedTax;
+    }
+
+    public String getProvTax() {
+        return provTax;
+    }
+
+    public void setProvTax(String provTax) {
+        this.provTax = provTax;
+    }
+
+    public String getCpp() {
+        return cpp;
+    }
+
+    public void setCpp(String cpp) {
+        this.cpp = cpp;
+    }
+
+    public String getEmpIns() {
+        return empIns;
+    }
+
+    public void setEmpIns(String empIns) {
+        this.empIns = empIns;
+    }
+
+    public String getRrsp() {
+        return rrsp;
+    }
+
+    public void setRrsp(String rrsp) {
+        this.rrsp = rrsp;
+    }
+
+    public String getFwdRRSP() {
+        return fwdRRSP;
+    }
+
+    public void setFwdRRSP(String fwdRRSP) {
+        this.fwdRRSP = fwdRRSP;
+    }
+
+    public String getTotalTaxableInc() {
+        return totalTaxableInc;
+    }
+
+    public void setTotalTaxableInc(String totalTaxableInc) {
+        this.totalTaxableInc = totalTaxableInc;
+    }
+
+    public String getTaxpayed() {
+        return taxpayed;
+    }
+
+    public void setTaxpayed(String taxpayed) {
+        this.taxpayed = taxpayed;
+    }
 }
--- app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/PersonInfoActivity.java
+++ app/src/main/java/com/kamal/c0777682_w2020_mad3125_midterm/PersonInfoActivity.java
@@ -3,35 +3,70 @@ package com.kamal.c0777682_w2020_mad3125_midterm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
-public class PersonInfoActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
-    private static final String TAG = PersonInfoActivity.class.getSimpleName();
-    int x = 100;
+public class PersonInfoActivity extends AppCompatActivity {
+    private TextInputEditText sin;
+    private TextInputEditText txtFname;
+    private TextInputEditText txtLname;
+    private TextInputEditText dob;
+    private RadioGroup rbgender;
+    private RadioButton rbmale;
+    private RadioButton rbfemale;
+    private TextInputEditText taxDate;
+    private TextInputEditText grossInc;
+    private TextInputEditText rrsp;
+    private Button btnClear;
+    private Button btnSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Person Information Form");
+        sin = findViewById(R.id.sin);
+        txtFname = findViewById(R.id.txtFname);
+        txtLname = findViewById(R.id.txtLname);
+        dob = findViewById(R.id.txtDate);
+        rbgender = findViewById(R.id.rbgender);
+        rbmale = findViewById(R.id.rbMale);
+        rbfemale = findViewById(R.id.rbFemale);
+        taxDate = findViewById(R.id.txtTaxDate);
+        grossInc = findViewById(R.id.txtGrossInc);
+        rrsp = findViewById(R.id.txtRRSP);
+        btnClear = findViewById(R.id.btnClear);
+        btnSubmit = findViewById(R.id.btnGoNext);
+        findViewById(R.id.btnDatePicker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    }
-
-        @Override
-        public void onConfigurationChanged(@NonNull Configuration newConfig)
-        {
-            super.onConfigurationChanged(newConfig);
-
-            if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
-            {
-                Log.d(TAG, "ORIENTATION_LANDSCAPE " + x );
-            }else
-            {
-                Log.d(TAG, "ORIENTATION_PORTRAIT " + x);
             }
+        });
+        private void showDatePickerDialog(){
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    this,this,
+
+        }
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(PersonInfoActivity.this,DataDisplayActivity.class);
+                startActivity(mIntent);
+            }
+        });
         }
     }
 
